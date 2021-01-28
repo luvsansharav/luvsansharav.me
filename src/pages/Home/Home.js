a092f5aeae7207d13952ef16e94e9e7056a853f1
luvsansharav <luvsansharav.a@gmail.com>
2021-01-27 17:43:32 -0800

    remove unnessary assets

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
-import React, { Component, Fragment } from "react";
-import { Timeline, Event } from "react-timeline-scribble";
+import React, { Component } from "react";
 
 class Home extends Component {
   constructor(props) {
@@ -34,51 +33,51 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="d-flex justify-content-center mt-3">
-          <p class="text-monospace font-weight-light">LOYA Altangerel</p>
-        </div>
-        <div className="d-flex justify-content-center">
-          {this.state.socialLinks.map(item => (
-            <a href={item.url} target="_blank" rel="noopener noreferrer">
-              <p class="text-monospace">{item.title}</p>
-            </a>
-          ))}
-        </div>
-        <div className="d-flex justify-content-center mt-3">
-          <p class="text-monospace">{this.state.currentTime.toString()}</p>
-        </div>
-        <div className="d-flex justify-content-center mt-5">
-          <Fragment>
-            {/* <h1>test</h1> */}
-            <Timeline>
-              <Event
-                interval={"May 24, 2019"}
-                title={"Who is Loya?"}
-                subtitle={"First post ever!"}
-              >
-                <p>
-                  My name is Loya, the second son of House Altangerel. I grew up
-                  in Mongolia its where my story begins. I am the one who loves
-                  to learn more about the computer science and share my
-                  experience.
-                </p>
-                <p>
-                  #Loya, #Altangerel, #second son, #House Altangerel, #Mongolia,
-                  #story, #begins #love to learn, #learn more, #computer
-                  science, #share, #experience
-                </p>
-                <audio
-                  controls
-                  src="https://s3-us-west-2.amazonaws.com/luvsansharav.me-audio-archive/whoisme_speech.mp3"
-                />
-                <footer className="blockquote-footer mt-4">
-                  <cite title="Source Title">Regards, Loya Altangerel</cite>
-                </footer>
-              </Event>
-            </Timeline>
-          </Fragment>
-        </div>
+      <div>
+        {/* Start Section */}
+        <section className="section pt-40 pb-0">
+          <div className="container">
+            <div className="hero__standout">
+              <img
+                src="assets/images/about-us.jpg"
+                className="img-fluid"
+                alt="Image article"
+              />
+
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="hero__standout--headline">
+                    <h2 className="section__heading">
+                      LOYA Altangerel{" "}
+                    </h2>
+                    <div className="headline">
+                      Entrepreneur, Tech Lead, Software Engineer, Solutions Architect, DevOps Engineer, Site Reliability Engineer, Analyst
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="hero__standout--content">
+                    <h4>Who is Loya?</h4>
+                    <p>
+                    My name is Loya, the second son of House Altangerel. 
+                    I grew up in Mongolia its where my story begins. 
+                    I am the one who loves to learn more about the computer science and share my experience.
+                    </p>
+                    <h4>Professional expertise</h4>
+                    <p>
+                    8+ years of professional software development experience:
+                    System design, development, problem solving, automation,
+                    distributed systems, monitoring, troubleshooting, testing,
+                    reliability, security, cost optimization, performance efficiency,
+                    operational excellence, capacity planning, fleet management, serverless application
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* End Section */}
       </div>
     );
   }
